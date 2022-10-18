3a8476a994350bd0c9ce5dcc35277865dc2e0b66
Adebanjo Adeniji <aeben.adebanjo@gmail.com>
2022-10-18 18:22:41 +0100

    deploy to netlify

--- index.js
+++ index.js
@@ -10,101 +10,25 @@ function checkLoginState() {
   });
 }
 
-("use strict");
+window.fbAsyncInit = function () {
+  FB.init({
+    appId: "{506092741548214}",
+    cookie: true,
+    xfbml: true,
+    version: "{api-version}",
+  });
 
-window.validateForm = function () {
-  return {
-    fullname: "",
-    email: "",
-    password: "",
-    validation: {
-      fullname: {
-        rule: {
-          required: function (field) {
-            if (field) {
-              return { error: false, message: "" };
-            } else {
-              return { error: true, message: "This field is required." };
-            }
-          },
-          minLength: function (field, value = 2) {
-            if (field && field.length >= value) {
-              return { error: false, message: "" };
-            } else {
-              return {
-                error: true,
-                message: `This field must have minimum ${value} characters length.`,
-              };
-            }
-          },
-        },
-        error: true,
-        message: "",
-      },
-      email: {
-        rule: {
-          required: function (field) {
-            if (field) {
-              return { error: false, message: "" };
-            } else {
-              return { error: true, message: "This field is required." };
-            }
-          },
-          email: function (field) {
-            const rgx =
-              /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-            if (rgx.test(field)) {
-              return { error: false, message: "" };
-            } else {
-              return {
-                error: true,
-                message: "This field has wrong email address format.",
-              };
-            }
-          },
-        },
-        error: true,
-        message: "",
-      },
-      password: {
-        rule: {
-          required: function (field) {
-            if (field) {
-              return { error: false, message: "" };
-            } else {
-              return { error: true, message: "This field is required." };
-            }
-          },
-          minLength: function (field, value = 8) {
-            if (field && field.length >= value) {
-              return { error: false, message: "" };
-            } else {
-              return {
-                error: true,
-                message: `This field must have minimum ${value} characters length.`,
-              };
-            }
-          },
-        },
-        error: true,
-        message: "",
-      },
-    },
-    validate(field) {
-      for (const key in this.validation[field].rule) {
-        const validationResult = this.validation[field].rule[key](this[field]);
-        if (validationResult.error) {
-          this.validation[field].error = true;
-          this.validation[field].message = validationResult.message;
-          break;
-        }
-        this.validation[field].error = false;
-        this.validation[field].message = "";
-        continue;
-      }
-    },
-  };
+  FB.AppEvents.logPageView();
 };
-// window.Alpine = Alpine;
 
-// Alpine.start();
+(function (d, s, id) {
+  var js,
+    fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {
+    return;
+  }
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+})(document, "script", "facebook-jssdk");
