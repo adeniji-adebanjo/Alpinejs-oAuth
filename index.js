548ea026fb573db2b83a7418af9c1c120b2217dc
Adebanjo Adeniji <aeben.adebanjo@gmail.com>
2022-10-18 16:16:57 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,110 @@
+// import Alpine from "alpinejs";
+<fb:login-button
+  scope="public_profile,email"
+  onlogin="checkLoginState();"
+></fb:login-button>;
+
+function checkLoginState() {
+  FB.getLoginStatus(function (response) {
+    statusChangeCallback(response);
+  });
+}
+
+("use strict");
+
+window.validateForm = function () {
+  return {
+    fullname: "",
+    email: "",
+    password: "",
+    validation: {
+      fullname: {
+        rule: {
+          required: function (field) {
+            if (field) {
+              return { error: false, message: "" };
+            } else {
+              return { error: true, message: "This field is required." };
+            }
+          },
+          minLength: function (field, value = 2) {
+            if (field && field.length >= value) {
+              return { error: false, message: "" };
+            } else {
+              return {
+                error: true,
+                message: `This field must have minimum ${value} characters length.`,
+              };
+            }
+          },
+        },
+        error: true,
+        message: "",
+      },
+      email: {
+        rule: {
+          required: function (field) {
+            if (field) {
+              return { error: false, message: "" };
+            } else {
+              return { error: true, message: "This field is required." };
+            }
+          },
+          email: function (field) {
+            const rgx =
+              /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            if (rgx.test(field)) {
+              return { error: false, message: "" };
+            } else {
+              return {
+                error: true,
+                message: "This field has wrong email address format.",
+              };
+            }
+          },
+        },
+        error: true,
+        message: "",
+      },
+      password: {
+        rule: {
+          required: function (field) {
+            if (field) {
+              return { error: false, message: "" };
+            } else {
+              return { error: true, message: "This field is required." };
+            }
+          },
+          minLength: function (field, value = 8) {
+            if (field && field.length >= value) {
+              return { error: false, message: "" };
+            } else {
+              return {
+                error: true,
+                message: `This field must have minimum ${value} characters length.`,
+              };
+            }
+          },
+        },
+        error: true,
+        message: "",
+      },
+    },
+    validate(field) {
+      for (const key in this.validation[field].rule) {
+        const validationResult = this.validation[field].rule[key](this[field]);
+        if (validationResult.error) {
+          this.validation[field].error = true;
+          this.validation[field].message = validationResult.message;
+          break;
+        }
+        this.validation[field].error = false;
+        this.validation[field].message = "";
+        continue;
+      }
+    },
+  };
+};
+// window.Alpine = Alpine;
+
+// Alpine.start();
